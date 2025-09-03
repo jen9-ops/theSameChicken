fa2c8bfde8ba738c4a81803c496f1e7cb50868da
jen9-ops <jenvovchuk@gmail.com>
2025-09-03 23:03:12 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,14 +1,12 @@
 importScripts('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest');
-
-let model;
-let progress = 0;
+let model, progress = 0;
 
 async function initModel() {
   model = tf.sequential();
   model.add(tf.layers.conv2d({ inputShape:[64,64,3], filters:8, kernelSize:5, activation:'relu' }));
   model.add(tf.layers.flatten());
   model.add(tf.layers.dense({ units:1, activation:'sigmoid' }));
-  model.compile({ loss: 'binaryCrossentropy', optimizer: 'adam' });
+  model.compile({ loss:'binaryCrossentropy', optimizer:'adam' });
 }
 
 self.onmessage = async e => {
@@ -16,16 +14,15 @@ self.onmessage = async e => {
     await initModel();
     trainingLoop();
   }
-  if (e.data.type === 'predict') {
+  else if (e.data.type === 'predict') {
     const tensor = tf.tensor(e.data.data, [1,64,64,3]);
     const pred = (await model.predict(tensor).data())[0];
-    self.postMessage({ type: 'pred', id: e.data.id, pred });
+    self.postMessage({ pred });
   }
 };
 
 async function trainingLoop() {
   for (let i = 1; i <= 100; i++) {
-    // training on fake constant "курица" data
     const xs = tf.randomNormal([8,64,64,3]);
     const ys = tf.ones([8,1]);
     await model.fit(xs, ys, { epochs:1, batchSize:8 });
